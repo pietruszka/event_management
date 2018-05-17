b9d94f71c8d22243b73fedede13d37075112d7fc
Kuba <1911kuba@gmail.com>
2018-05-17 13:40:46 +0200

    Created user controller, added bcrypt and jsonwebtoken, implemented local registration and login

--- src/user/routes.js
+++ src/user/routes.js
@@ -1,9 +1,12 @@
 const {Router} = require('express')
+const usersController = require('./controller.js');
 
 class UserRouter {
     constructor() {
         this.router = Router();
 
+        this.router.post('/auth/local/register', usersController.registerUser);
+        this.router.post('/auth/local/login', usersController.authenticateUser);
     }
 
     getRouter() {
